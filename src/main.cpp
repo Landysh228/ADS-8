bb290e8c88ea2f62a4177314f83c93d9ade6ec5f
Landysh <walfa45milana@mail.ru>
2026-05-30 14:04:23 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,13 @@
 // Copyright 2021 NNTU-CS
+#include <string>
 #include "bst.h"
 
+void makeTree(BST<std::string> &tree, const char *filenick);
+void printFreq(BST<std::string> &tree);
+
 int main() {
-    return 0;
+  BST<std::string> dictionary;
+  makeTree(dictionary, "src/war_peace.txt");
+  printFreq(dictionary);
+  return 0;
 }
